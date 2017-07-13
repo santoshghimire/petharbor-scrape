31767f205250204255d24827db2492762c5c77cb
Santosh Ghimire <santosh.ghimire33@gmail.com>
2017-07-13 20:24:11 +0545

    matching algorithm update

--- petharbor/matching_algorithm.py
+++ petharbor/matching_algorithm.py
@@ -27,12 +27,12 @@ class GetMatchPercentage(object):
         """
         # Criteria for the matching
         criteria = {
-            'last_seen': 10,
-            'species': 10,
-            'breed': 20,
-            'color': 30,
-            'gender': 20,
-            'name': 10,
+            'last_seen': 0,
+            'species': 0,
+            'breed': 30,
+            'color': 25,
+            'gender': 40,
+            'name': 5,
         }
 
         # convert last seen date and brought to shelter dates
@@ -54,25 +54,25 @@ class GetMatchPercentage(object):
             return 0
 
         # compare the breed
-        if self.match(self.lost_pet['breed'], item['breed']):
-            breed_match = 1
-        else:
-            breed_match = self.similar(self.lost_pet['breed'], item['breed'])
+        breed_match = self.match(
+            self.lost_pet['breed'], item['breed'], breed=True)
 
         # compare the color
+        main_color = self.get_transformed_color(item['main_color'])
         color_match = self.match(
-            item['main_color'], self.lost_pet['description'])
+            main_color, self.lost_pet['description'], color=True)
 
-        if self.match(self.lost_pet['sex'], item['gender'], gender=True):
-            gender_match = 1
-        else:
-            gender_match = self.similar(self.lost_pet['sex'], item['gender'])
+        # compare the gender
+        gender_match = self.match(
+            self.lost_pet['sex'], item['gender'], gender=True)
 
+        # compare the pet name
         if self.match(self.lost_pet['name'], item['name']):
             name_match = 1
         else:
             name_match = self.similar(self.lost_pet['name'], item['name'])
 
+        # calculate the final score
         final_score = criteria['last_seen'] * last_seen_match + \
             criteria['species'] * species_match +\
             criteria['breed'] * breed_match + \
@@ -84,7 +84,7 @@ class GetMatchPercentage(object):
     def similar(self, a, b):
         return SequenceMatcher(None, a, b).ratio()
 
-    def match(self, a, b, gender=False):
+    def match(self, a, b, gender=False, breed=False, color=False):
         if gender:
             # special case of gender
             if 'female' in a.lower() and 'female' in b.lower():
@@ -99,7 +99,24 @@ class GetMatchPercentage(object):
             elif 'female' in a.lower() and 'male' in b.lower():
                 return 0
             else:
+                return 1
+        if breed:
+            a = a.lower().replace(" ", "")
+            b = b.lower().replace(" ", "")
+            if a == b:
+                return 1
+            elif a in b or b in a:
+                return 1
+            else:
                 return 0
+        if color:
+            # match each words in color
+            color_score = 0
+            colors = a.split(' ')
+            for c in colors:
+                if c.lower() in b.lower():
+                    color_score += 1.0 / len(colors)
+            return color_score
         if (
             a.lower() in b.lower() or
             b.lower() in a.lower()
@@ -108,8 +125,17 @@ class GetMatchPercentage(object):
         else:
             return 0
 
+    def get_transformed_color(self, main_color):
+        main_color = main_color.lower()
+        main_color = main_color.replace('org', 'orange')
+        main_color = main_color.replace('brn', 'brown')
+        return main_color
+
 if __name__ == '__main__':
     m = GetMatchPercentage('')
-    per = m.match('Female', "Gender Unknown", gender=True)
-    print(per)
-    print(m.similar('Female', 'Gender Unknown'))
+    # per = m.match('Male', "Gender Unknown", gender=True)
+    # print(per)
+    # print(m.match('Domestic Short Hair', 'Domestic Shorthair', breed=True))
+    main_color = m.get_transformed_color('Brn Tabby')
+    print(main_color)
+    print(m.match(main_color, 'Effy is a brown cat', color=True))
